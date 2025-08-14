ede22c21f464c5aca4e00a93243c7683fcae56d4
C203-08 <C203-08@C203-08>
2025-08-14 09:00:47 +0800

    code na to

--- LESSON1/Form1.cs
+++ LESSON1/Form1.cs
@@ -19,77 +19,93 @@ namespace LESSON1
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Adobo";
+            priceTxtbox.Text = "120";
         }
 
         private void pictureBox4_Click(object sender, EventArgs e)
         {
+            itemnameTextbox.Text = "Menudo";
+            priceTxtbox.Text = "150";
 
         }
 
         private void pictureBox5_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Shanghai";
+            priceTxtbox.Text = "80";
         }
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Lechon Baboy";
+            priceTxtbox.Text = "150";
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Sinigang";
+            priceTxtbox.Text = "100";
         }
 
         private void pictureBox7_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Pork Sisig";
+            priceTxtbox.Text = "85";
         }
 
         private void pictureBox8_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Bicol Express";
+            priceTxtbox.Text = "90";
         }
 
         private void pictureBox9_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Kare-Kare";
+            priceTxtbox.Text = "110";
         }
 
         private void pictureBox10_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Pancit Bihon";
+            priceTxtbox.Text = "70";
         }
 
         private void pictureBox11_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Beef Nilaga";
+            priceTxtbox.Text = "120";
         }
 
         private void pictureBox12_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Pork Siomai";
+            priceTxtbox.Text = "65";
         }
 
         private void pictureBox13_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Barbeque";
+            priceTxtbox.Text = "50";
         }
 
         private void pictureBox14_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Kaldereta";
+            priceTxtbox.Text = "120";
         }
 
         private void pictureBox15_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Tinola";
+            priceTxtbox.Text = "100";
         }
 
         private void pictureBox16_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Text = "Pinakbet";
+            priceTxtbox.Text = "110";
         }
 
         private void itemnameTextbox_TextChanged(object sender, EventArgs e)
@@ -104,12 +120,13 @@ namespace LESSON1
 
         private void newBtn_Click(object sender, EventArgs e)
         {
-
+            itemnameTextbox.Clear();
+            priceTxtbox.Clear();
         }
 
         private void exitBtn_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
